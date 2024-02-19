141283ed3ed9c66ce886d035bef98fa25e550e9e
ghaefner <guillaume-haefner@gmx.de>
2024-02-19 20:02:22 +0100

    Adjust read_data function to transform smoker values into boolean

--- icc/src/api.py
+++ icc/src/api.py
@@ -1,5 +1,7 @@
 from pandas import read_csv, DataFrame
-from config import Config
+from config import Config, Columns
 
 def read_data(file_path=Config.PATH_TO_DATA) -> DataFrame:
-    return read_csv(file_path)
+    df = read_csv(file_path)
+    df[Columns.SMOKER] = df[Columns.SMOKER].apply(lambda x: 1 if x=='yes' else 0)
+    return df
