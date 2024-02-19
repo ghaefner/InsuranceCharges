08f6a580b89aa255ae838808e994e565da409d27
ghaefner <guillaume-haefner@gmx.de>
2024-02-19 20:15:37 +0100

    Create a Task Class for EDA

--- icc/src/plot.py
+++ icc/src/plot.py
@@ -12,6 +12,7 @@ def plot_average_bmi_sex(df):
     Returns:
     - None (only saves figure)
     """
+    print("[I] Plotting Average BMI by Sex.")
     fig, ax = plt.subplots() 
 
     barplot(data=df, x=Columns.SEX, y=Columns.BMI, ax=ax)
@@ -22,6 +23,7 @@ def plot_average_bmi_sex(df):
     
     fig.savefig(Config.PATH_TO_PLOT_FOLDER+"average_bmi_sex.png")
     plt.close()
+    print("[I] Done.")
 
 def plot_smoker_rate(df):
     """
@@ -33,6 +35,7 @@ def plot_smoker_rate(df):
     Returns:
     - None (only saves figure)
     """
+    print("[I] Plotting Smoker Rate.")
     df_smoker = df.groupby([Columns.SEX, Columns.SMOKER]).size().unstack()
     df_sex = df.groupby(Columns.SEX).size().to_frame(name="total")
     df_ratio = df_smoker.div(df_sex['total'], axis=0)
@@ -46,6 +49,7 @@ def plot_smoker_rate(df):
 
     ax.figure.savefig(Config.PATH_TO_PLOT_FOLDER+'smokers_sex.png')
     plt.close()
+    print("[I] Done.")
 
 
 def plot_charge_distribution_sex(df):
@@ -58,6 +62,7 @@ def plot_charge_distribution_sex(df):
     Returns:
     - None (only saves figure)
     """    
+    print("[I] Plotting Charge Distribution by Sex.")
     _, axs = plt.subplots(1, 2, figsize=(8, 4))
 
     histplot(data=df[df[Columns.SEX] == 'male'], x=Columns.FACT, kde=True, ax=axs[0], color='purple')
@@ -68,6 +73,7 @@ def plot_charge_distribution_sex(df):
 
     plt.savefig(Config.PATH_TO_PLOT_FOLDER+'charges_dist_sex.png')
     plt.close()
+    print("[I] Done.")
 
 
 def plot_bmi_distribution(df):
@@ -79,7 +85,8 @@ def plot_bmi_distribution(df):
 
     Returns:
     - None (only saves figure)
-    """    
+    """
+    print("[I] Plotting BMI Distribution by Sex.")
     _, axs = plt.subplots(1, 2, figsize=(8, 4))
 
     histplot(data=df[df[Columns.SEX] == 'male'], x=Columns.BMI, kde=True, ax=axs[0], color='purple')
@@ -90,6 +97,7 @@ def plot_bmi_distribution(df):
 
     plt.savefig(Config.PATH_TO_PLOT_FOLDER+'bmi_dist_sex.png')
     plt.close()
+    print("[I] Done.")
     
 
 def plot_bmi_scatter_smoker(df):
@@ -102,13 +110,14 @@ def plot_bmi_scatter_smoker(df):
     Returns:
     - None (only saves figure)
     """
-    
+    print("[I] Plotting BMI vs. Charge Rate by Smoker/Non-Smoker.")
     _, ax = plt.subplots()
     scatterplot(data=df, x='bmi', y='charges', ax=ax, hue='smoker')
     ax.set_ylabel('Charges [USD]')
     ax.set_xlabel('BMI [kg/m²]')
     ax.set_title('Charges vs. BMI Plot for Smokers and Non-Smokers')
     plt.savefig(Config.PATH_TO_PLOT_FOLDER+"bmi_scatter_smoker.png")
+    print("[I] Done.")
 
 
 def plot_nchildren(df):
@@ -121,6 +130,7 @@ def plot_nchildren(df):
     Returns:
     - None (only saves figure)
     """
+    print("[I] Plotting Number of Children.")
     df_children = df.groupby(Columns.KIDS).size().to_frame(name="total")
     ax = df_children.plot(kind='bar', stacked=False, legend=False)
 
@@ -129,6 +139,7 @@ def plot_nchildren(df):
     ax.set_xticklabels(ax.get_xticklabels(), rotation=0) # Force rotation to be zero, 90 default
 
     ax.figure.savefig(Config.PATH_TO_PLOT_FOLDER+"number_of_children.png")
+    print("[I] Done.")
     
 def plot_children_by_region(df):
     """
@@ -140,7 +151,7 @@ def plot_children_by_region(df):
     Returns:
     - None (only saves figure)
     """
-
+    print("[I] Plotting Average Number of Children for Different Regions.")
     _, ax = plt.subplots()
     barplot(data=df, y='region', x='children', ax=ax, palette='Set1', ci=None)
     ax.set_title("Average No. of Children for Different Regions")
@@ -150,6 +161,7 @@ def plot_children_by_region(df):
         ax.bar_label(container, label_type='edge', fmt='%.1f')    
 
     ax.figure.savefig(Config.PATH_TO_PLOT_FOLDER+"avg_children_by_region.png")
+    print("[I] Done.")
 
 def plot_charge_by_region(df):
     """
@@ -161,6 +173,7 @@ def plot_charge_by_region(df):
     Returns:
     - None (only saves figure)
     """
+    print("[I] Plotting Average Charges by Region.")
     _, ax = plt.subplots()
     barplot(data=df, y=Columns.LOC, x=Columns.FACT, ax=ax, palette='Set1', ci=None)
     ax.set_title("Average Charge for Different Regions")
@@ -170,3 +183,23 @@ def plot_charge_by_region(df):
         ax.bar_label(container, label_type='edge', fmt='${:,.0f}')    
 
     ax.figure.savefig(Config.PATH_TO_PLOT_FOLDER+"avg_charge_by_region.png")
+    print("[I] Done.")
+
+class Task_EDA:
+    def __init__(self, df):
+        self.df = df
+
+    def execute(self):
+        print("[I]: Info\n [W]: Warning\n [E]: Error")
+        print("[I] Starting Exploratory Data Analysis.")
+        
+        plot_average_bmi_sex(self.df)
+        plot_smoker_rate(self.df)
+        plot_charge_distribution_sex(self.df)
+        plot_bmi_distribution(self.df)
+        plot_bmi_scatter_smoker(self.df)
+        plot_nchildren(self.df)
+        plot_children_by_region(self.df)
+        plot_charge_by_region(self.df)
+
+        print("[I] Exploratory Data Analysis (EDA) completed.")
