89f4bd4701af1b09383668e6d58ac935b118b04f
ghaefner <guillaume-haefner@gmx.de>
2024-02-19 20:01:45 +0100

    Add config files

--- icc/config.py
+++ icc/config.py
@@ -1,3 +1,14 @@
 
 class Config:
     PATH_TO_DATA = "icc/data/insurance.csv"
+    PATH_TO_PLOT_FOLDER = "icc/plots/"
+
+class Columns:
+    ID = "index"
+    AGE = "age"
+    SEX = "sex"
+    BMI = "bmi"
+    KIDS = "children"
+    SMOKER = "smoker"
+    LOC = "region"
+    FACT = "charges"
