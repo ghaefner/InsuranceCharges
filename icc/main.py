445ed90b092db337ae1d3a281f87e42c660997fd
ghaefner <guillaume-haefner@gmx.de>
2024-02-19 21:25:05 +0100

    Initial main file commit

--- icc/main.py
+++ icc/main.py
@@ -1,5 +1,15 @@
-from src.api import read_data
+from icc.src.model import Task_Model
+from icc.src.plot import Task_EDA
+from icc.src.api import read_data
+from icc.config import HyperPars
 
-df = read_data()
+# Initialize Dataframe
+global _dataframe
+_dataframe = read_data()
 
-print(df.head())
+# Initialize Task Classes
+task_eda = Task_EDA(df=_dataframe)
+task_model = Task_Model(df=_dataframe)
+
+task_eda.run()
+task_model.run(hyperparameters=HyperPars)
