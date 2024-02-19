caa916934b2e1910f7ca471924745932028dfea9
ghaefner <guillaume-haefner@gmx.de>
2024-02-19 20:02:46 +0100

    Add plot functions and plots for EDA

---
+++ icc/src/plot.py
@@ -0,0 +1,172 @@
+import matplotlib.pyplot as plt
+from seaborn import barplot, histplot, scatterplot
+from config import Config, Columns
+
+def plot_average_bmi_sex(df):
+    """
+    Plots the average BMI for each sex.
+
+    Parameters:
+    - df (DataFrame): DataFrame containing BMI and sex data.
+
+    Returns:
+    - None (only saves figure)
+    """
+    fig, ax = plt.subplots() 
+
+    barplot(data=df, x=Columns.SEX, y=Columns.BMI, ax=ax)
+    ax.bar_label(ax.containers[0], fmt=f'%.2f')
+    ax.set_ylabel('BMI (kg/m²)')
+    ax.set_ylim((0, 50))
+    ax.set_title("Average BMI for each Sex")
+    
+    fig.savefig(Config.PATH_TO_PLOT_FOLDER+"average_bmi_sex.png")
+    plt.close()
+
+def plot_smoker_rate(df):
+    """
+    Plots the percentage of smokers by sex.
+
+    Parameters:
+    - df (DataFrame): DataFrame containing smoker and sex data.
+
+    Returns:
+    - None (only saves figure)
+    """
+    df_smoker = df.groupby([Columns.SEX, Columns.SMOKER]).size().unstack()
+    df_sex = df.groupby(Columns.SEX).size().to_frame(name="total")
+    df_ratio = df_smoker.div(df_sex['total'], axis=0)
+    
+    ax = df_ratio.plot(kind='bar', stacked=True)
+    ax.set_ylabel('% of Individuals')
+    ax.set_title('Stacked Bar Chart of Smokers by Sex')
+
+    for container in ax.containers:
+        ax.bar_label(container, label_type='center', fmt='%.2f%%')
+
+    ax.figure.savefig(Config.PATH_TO_PLOT_FOLDER+'smokers_sex.png')
+    plt.close()
+
+
+def plot_charge_distribution_sex(df):
+    """
+    Plots the distribution of charges by sex.
+
+    Parameters:
+    - df (DataFrame): DataFrame containing charge and sex data.
+
+    Returns:
+    - None (only saves figure)
+    """    
+    _, axs = plt.subplots(1, 2, figsize=(8, 4))
+
+    histplot(data=df[df[Columns.SEX] == 'male'], x=Columns.FACT, kde=True, ax=axs[0], color='purple')
+    histplot(data=df[df[Columns.SEX] == 'female'], x=Columns.FACT, kde=True, ax=axs[1], color='red')
+
+    axs[0].set_title("For Males")
+    axs[1].set_title("For Females")
+
+    plt.savefig(Config.PATH_TO_PLOT_FOLDER+'charges_dist_sex.png')
+    plt.close()
+
+
+def plot_bmi_distribution(df):
+    """
+    Plots the distribution of BMI by sex.
+
+    Parameters:
+    - df (DataFrame): DataFrame containing BMI and sex data.
+
+    Returns:
+    - None (only saves figure)
+    """    
+    _, axs = plt.subplots(1, 2, figsize=(8, 4))
+
+    histplot(data=df[df[Columns.SEX] == 'male'], x=Columns.BMI, kde=True, ax=axs[0], color='purple')
+    histplot(data=df[df[Columns.SEX] == 'female'], x=Columns.BMI, kde=True, ax=axs[1], color='red')
+
+    axs[0].set_title("For Males")
+    axs[1].set_title("For Females")
+
+    plt.savefig(Config.PATH_TO_PLOT_FOLDER+'bmi_dist_sex.png')
+    plt.close()
+    
+
+def plot_bmi_scatter_smoker(df):
+    """
+    Plots a scatter plot of charges vs. BMI for smokers and non-smokers.
+
+    Parameters:
+    - df (DataFrame): DataFrame containing BMI, charges, and smoker data.
+
+    Returns:
+    - None (only saves figure)
+    """
+    
+    _, ax = plt.subplots()
+    scatterplot(data=df, x='bmi', y='charges', ax=ax, hue='smoker')
+    ax.set_ylabel('Charges [USD]')
+    ax.set_xlabel('BMI [kg/m²]')
+    ax.set_title('Charges vs. BMI Plot for Smokers and Non-Smokers')
+    plt.savefig(Config.PATH_TO_PLOT_FOLDER+"bmi_scatter_smoker.png")
+
+
+def plot_nchildren(df):
+    """
+    Plots the distribution of the number of children.
+
+    Parameters:
+    - df (DataFrame): DataFrame containing children data.
+
+    Returns:
+    - None (only saves figure)
+    """
+    df_children = df.groupby(Columns.KIDS).size().to_frame(name="total")
+    ax = df_children.plot(kind='bar', stacked=False, legend=False)
+
+    ax.set_ylabel('Number of Individuals')
+    ax.set_title('Bar Chart for Number of Children')
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=0) # Force rotation to be zero, 90 default
+
+    ax.figure.savefig(Config.PATH_TO_PLOT_FOLDER+"number_of_children.png")
+    
+def plot_children_by_region(df):
+    """
+    Plots the average number of children for different regions.
+
+    Parameters:
+    - df (DataFrame): DataFrame containing region and children data.
+
+    Returns:
+    - None (only saves figure)
+    """
+
+    _, ax = plt.subplots()
+    barplot(data=df, y='region', x='children', ax=ax, palette='Set1', ci=None)
+    ax.set_title("Average No. of Children for Different Regions")
+    ax.set_xlim((0, 1.5))
+
+    for container in ax.containers:
+        ax.bar_label(container, label_type='edge', fmt='%.1f')    
+
+    ax.figure.savefig(Config.PATH_TO_PLOT_FOLDER+"avg_children_by_region.png")
+
+def plot_charge_by_region(df):
+    """
+    Plots the average charge for different regions.
+
+    Parameters:
+    - df (DataFrame): DataFrame containing region and charge data.
+
+    Returns:
+    - None (only saves figure)
+    """
+    _, ax = plt.subplots()
+    barplot(data=df, y=Columns.LOC, x=Columns.FACT, ax=ax, palette='Set1', ci=None)
+    ax.set_title("Average Charge for Different Regions")
+    ax.set_xlim((0, 19000))
+
+    for container in ax.containers:
+        ax.bar_label(container, label_type='edge', fmt='${:,.0f}')    
+
+    ax.figure.savefig(Config.PATH_TO_PLOT_FOLDER+"avg_charge_by_region.png")
