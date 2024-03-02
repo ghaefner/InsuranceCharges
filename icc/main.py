d12fdf0e7067fd54b3dfc6be198b187c0f246940
ghaefner <guillaume-haefner@gmx.de>
2024-03-02 10:11:23 +0100

    Adjust main file to load new classes

--- icc/main.py
+++ icc/main.py
@@ -1,16 +1,14 @@
 from src.model import TaskModel
 from src.plot import TaskEDA
-from src.api import read_data
-from config import HyperPars
-
-# Initialize Dataframe
-global _dataframe
-_dataframe = read_data()
+from config import HyperPars, Config
 
 # Initialize Task Classes
-task_eda = TaskEDA(df=_dataframe)
-task_model = TaskModel(df=_dataframe)
+task_eda = TaskEDA(conf=Config)
+task_model = TaskModel(conf=Config)
 
-# task_eda.run()
+# Run plot routine
+task_eda.run()
+
+# Run model fits
 task_model.run(hyper_parameters=HyperPars)
 
