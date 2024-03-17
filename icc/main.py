0a8ab232795ec9573b5c975953cb58a1200c3b27
ghaefner <guillaume-haefner@gmx.de>
2024-03-17 10:34:47 +0100

    Test predict charge method

--- icc/main.py
+++ icc/main.py
@@ -1,14 +1,18 @@
 from src.model import TaskModel
 from src.plot import TaskEDA
+from src.person import Person
 from config import HyperPars, Config
 
 # Initialize Task Classes
-task_eda = TaskEDA(conf=Config)
+# task_eda = TaskEDA(conf=Config)
 task_model = TaskModel(conf=Config)
 
 # Run plot routine
-task_eda.run()
+# task_eda.run()
 
 # Run model fits
-task_model.run(hyper_parameters=HyperPars)
+# task_model.run(hyper_parameters=HyperPars)
+mod = task_model.get_train_model()
 
+p1 = Person(25, "male", 24.5, 0, False, "southwest")
+p1.predict_charges(mod)
