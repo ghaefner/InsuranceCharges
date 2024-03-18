306f96b5b7d4b631293a15183bd106cfb7fc2965
ghaefner <guillaume-haefner@gmx.de>
2024-03-18 20:43:49 +0100

    Adjust main module

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
-from icc.src.model import TaskModel, evaluate_model
-from icc.src.plot import TaskEDA
+from icc.src.model import Model
+from icc.src.plot import Plot
 from icc.src.person import Person
 from icc.config import HyperPars, Config, Columns
 import pandas as pd
 
 # Initialize Task Classes
-# task_eda = TaskEDA(conf=Config)
-task_model = TaskModel(conf=Config)
+plot = Plot(conf=Config)
+model = Model(conf=Config)
 
 # Run plot routine
-# task_eda.run()
+plot.run()
 
 # Run model fits
-# task_model.run(hyper_parameters=HyperPars)
-mod = task_model.get_train_model()
+model.run(hyper_parameters=HyperPars)
 
-print(mod)
-
+# Create a User and predict charges
 p1 = Person(25, "male", 24.5, 0, False, "southeast")
-# p1.predict_charges(mod)
-
-print(p1.predict_charges(mod))
+p1.predict_charges(model.get_train_model())
